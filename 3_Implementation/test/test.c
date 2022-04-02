ddc67a87e33a45dc8038be63f2e0df0203f88f85
Himanshu Panchbhai <himanshu.panchbhai3006@gmail.com>
2022-04-02 23:51:28 +0530

    test.c

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,19 @@
+#include "unity.h"
+#include "record.h"
+int main()
+{
+  fp = fopen("data.txt", "rb+");
+
+	if (fp == NULL) 
+	{
+		fp = fopen("data.txt", "wb+");
+	}
+  
+UNITY_BEGIN();
+
+  RUN_TEST(test_addrecord);
+  RUN_TEST(test_deleterecord);
+  RUN_TEST(test_modifyrecord);
+
+  return UNITY_END();
+}
